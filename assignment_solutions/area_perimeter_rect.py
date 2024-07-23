a2edfb10d0207780e11d8b1baf8cd51612e7c9fa
ddeepak8894 <ddeepak8894@gmail.com>
2024-07-23 22:32:57 +0530

    moved avg file and calculated rectangle

---
+++ assignment_solutions/area_perimeter_rect.py
@@ -0,0 +1,7 @@
+length = float(input("the length of rectangle is:"))
+print(length)
+breadth = float(input("the breadth of rectangle is:"))
+print(breadth)
+area_of_rectangle= length*breadth
+perimeter_of_rectangle=2*(length+breadth)
+print(f"the area and perimeter of rectangle is {area_of_rectangle},{perimeter_of_rectangle}")
